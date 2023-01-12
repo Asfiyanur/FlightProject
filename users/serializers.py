46dd26baac191caa1cd20af2f2e642fb89657684
Asfiyanur <nurasfiya.13@gmail.com>
2023-01-12 22:31:18 +0300

    loginUserData

--- users/serializers.py
+++ users/serializers.py
@@ -3,13 +3,14 @@ from django.contrib.auth.models import User
 from rest_framework.validators import UniqueValidator
 from django.contrib.auth.password_validation import validate_password
 
+from dj_rest_auth.serializers import TokenSerializer
 
 class RegisterSerializer(serializers.ModelSerializer):
-    
-    email =serializers.EmailField(
+
+    email = serializers.EmailField(
         required=True,
         validators = [UniqueValidator(queryset=User.objects.all())]
-    )
+        )
     
     password = serializers.CharField(
         write_only = True,
@@ -17,26 +18,22 @@ class RegisterSerializer(serializers.ModelSerializer):
         validators = [validate_password],
         style = {"input_type" : "password"}
     )
-    
     password2 = serializers.CharField(
         write_only = True,
         required = True,
         style = {"input_type" : "password"}
     )
     
-    
     class Meta:
-        model=User
-        fields=[
+        model = User
+        fields = [
         'username',
         'first_name',
         'last_name',
         'email',
         'password',
-        'password2'
-        ]
-
-
+        'password2']
+        
     def validate(self, data):
         if data["password"] != data["password2"]:
             raise serializers.ValidationError(
@@ -52,3 +49,17 @@ class RegisterSerializer(serializers.ModelSerializer):
         user.set_password(password)
         user.save()
         return user
+    
+    
+class UserTokenSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ("id", "first_name", "last_name", "email")
+    
+    
+class CustomTokenSerializer(TokenSerializer):
+    user = UserTokenSerializer(read_only = True)
+    
+    class Meta(TokenSerializer.Meta):
+        fields = ("key", "user")
+        
