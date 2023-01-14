4e474c2e5fed2686caee6164c04ae2bb3c0b78ae
Asfiyanur <nurasfiya.13@gmail.com>
2023-01-14 11:47:51 +0300

    permissionUserCRud

--- flight/serializers.py
+++ flight/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Flight
+from .models import Flight,Reservation
 
 class FlightSerializer(serializers.ModelSerializer):
     
@@ -15,4 +15,10 @@ class FlightSerializer(serializers.ModelSerializer):
             "etd"
         )
         
+
+class ReservationSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Reservation
+        fields = '__all__'
         
